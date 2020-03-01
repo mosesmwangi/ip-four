caa5b95a290311e455f5109586b0e1b8205df4d0
mosesmwangi <mosesmwangi286@gmail.com>
2020-03-02 02:12:59 +0300

    add the logic for getting price of topping(s)

--- js/scripts.js
+++ js/scripts.js
@@ -18,4 +18,50 @@ $(document).ready(function() {
         $("#prizeTwo").text(showPriceTwo);
     })
 
+    $("button#topping").click(function () {
+        var priceOfTopping = [];
+        $.each($("input[name='topping']:checked"), function () {
+            priceOfTopping.push($(this).val());
+        });
+        // var allPriceOfTopping = priceOfTopping.join(",");
+        // console.log(allPriceOfTopping);
+        var totalSumOfToppings = 0;
+        // $("#prizeThree").text(priceOfTopping.join(","));
+        // console.log(priceOfTopping);
+
+        for (var index = 0; index < priceOfTopping.length; index++) {
+
+            // totalSumOfToppings = 0;
+
+            totalSumOfToppings += parseInt(priceOfTopping[index]);
+            $("#prizeOfTopping").text(totalSumOfToppings);
+        }
+
+        // $("button#btn").click(function () {
+        //     var showPrice = $("select#size").children("option:selected").val();
+        //     var showPriceNow = parseInt(showPrice);
+        //     var showPriceTwo = $("select#crust").children("option:selected").val();
+        //     var showPriceTwoNow = parseInt(showPriceTwo);
+        //     var priceOfTopping = [];
+        //     $.each($("input[name='topping']:checked"), function () {
+        //         priceOfTopping.push($(this).val());
+        //     });
+        //     var numValue = $("input#number").val();
+        //     var nuumValue = parseInt(numValue);
+        //     var totalSumOfToppings = 0;
+        //     // console.log(priceOfTopping);
+
+        //     for (var index = 0; index < priceOfTopping.length; index++) {
+
+        //         totalSumOfToppings += parseInt(priceOfTopping[index]);
+        //         // console.log(totalSumOfToppings);
+        //     }
+        //     console.log((showPriceNow + showPriceTwoNow + totalSumOfToppings) * nuumValue);
+            
+        //     // console.log();
+        // })
+
+    })
+
+
 });
